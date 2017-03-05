d97611c3f7b4da608160ae8e1759ff9198d687ad
gambit6521 <gambit6521@gmail.com>
2017-03-04 17:38:15 -0800

    Add files via upload

---
+++ AreaAndPerimeterOfaCircle.java
@@ -0,0 +1,24 @@
+package textexercisesweek1;
+
+/**
+ *
+ * @author Ryan
+ */
+public class AreaAndPerimeterOfaCircle {
+
+       public static void main(String[] args) {
+            // displays area and perimeter of a circle that has a radius of 5.5
+            
+       double radius = 5.5;
+       
+       double perimeter = 2 * radius * Math.PI;
+       
+       double area = radius * radius * Math.PI;
+       
+       System.out.println(area);
+       
+       System.out.println(perimeter);
+       
+       }
+
+}
