c8f4d6349091ca34167d083c86c47f0ec883e5f9
gambit6521 <gambit6521@gmail.com>
2017-03-04 18:14:30 -0800

    Add files via upload

---
+++ ApproximatePi.java
@@ -0,0 +1,15 @@
+package Topic1;
+
+public class ApproximatePi {
+
+	public static void main(String[] args) {
+		// TODO approximate pi with formula
+		// Pi = 4 * (1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11 + ...)
+				
+		System.out.println(4 * (1.0 - 1.0/3 + 1.0/5 - 1.0/7 + 1.0/9 - 1.0/11));
+
+		System.out.println(4 * (1.0 - 1.0/3 + 1.0/5 - 1.0/7 + 1.0/9 - 1.0/11 + 1.0/13));
+	
+		}
+
+}
